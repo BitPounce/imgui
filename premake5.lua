63c7f6ce2960a4d13574bd0cf78884bf688128aa
Cats Studio <79156677+iojuedioe9rd@users.noreply.github.com>
2025-12-28 21:57:01 +0000

    added premake

---
+++ premake5.lua
@@ -0,0 +1,37 @@
+project "ImGui"
+	kind "StaticLib"
+	language "C++"
+
+    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+    files
+    {
+        "imconfig.h",
+        "imgui_demo.cpp",
+        "imgui_draw.cpp",
+        "imgui_internal.h",
+        "imgui_tables.cpp",
+        "imgui_widgets.cpp",
+        "imgui.cpp",
+        "imgui.h",
+        "imstb_rectpack.h",
+        "imstb_textedit.h",
+        "imstb_truetype.h"
+    }
+
+    filter "system:windows"
+		systemversion "latest"
+		cppdialect "C++17"
+		staticruntime "On"
+
+    filter "system:linux"
+		pic "On"
+		systemversion "latest"
+		cppdialect "C++17"
+		staticruntime "On"
+
+    filter "configurations:Debug"
+        runtime "Debug"
+        symbols "On"
+    
